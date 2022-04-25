3ee514e5ad97d53d335e0aa013aecc40dbae64bb
victorCodeDev <ojedavictor80@gmail.com>
2022-04-25 11:20:08 -0400

    Se terminó de implementar servicio soap

--- src/com/vojeda/calculadora/Main.java
+++ src/com/vojeda/calculadora/Main.java
@@ -6,6 +6,7 @@ import javax.xml.ws.Endpoint;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Endpoint.publish("http://localhost:1516/WS/Calculadora", new Operacionimpl());
 	}
 
